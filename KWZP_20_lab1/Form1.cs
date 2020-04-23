f218c9f3dceb6563952f223825cf37b5e27b0f2d
Piotr Wąsik <piotr.wasik@student.wat.edu.pl>
2020-04-23 17:08:45 +0200

    Mój program z dzisiejszych laborek

---
+++ KWZP_20_lab1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KWZP_20_lab1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			if (textBox1.TextLength > 0)
+			{
+				btnChange.Text = textBox1.Text;
+			}
+		}
+
+		private void chcBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			btnChange.Enabled = chcBox1.Checked;
+		}
+
+		private void btnChange_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
